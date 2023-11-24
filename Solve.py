1a673099e433cbb439a7f0b3575bf219fbe5d9ca
Serafim <serafim@MacBook-Pro-Serafim.local>
2023-11-25 00:35:13 +0300

    добавил файлы

---
+++ Solve.py
@@ -0,0 +1,72 @@
+class Solver:
+    """
+    Класс Solver представляет собой объект для поиска пути в лабиринте.
+
+    Attributes:
+    - maze (Maze): Экземпляр класса Maze, представляющий лабиринт.
+    - path (list): Двумерный список, представляющий длины пути от каждой ячейки до конечной точки.
+
+    Methods:
+    - solve(self, x1, y1, x2, y2): Использует глубинный поиск для нахождения кратчайшего пути от (x1, y1) до (x2, y2).
+    """
+
+    def __init__(self, maze):
+        """
+        Инициализирует экземпляр класса Solver.
+
+        Parameters:
+        - maze (Maze): Экземпляр класса Maze, представляющий лабиринт.
+        """
+        self.maze = maze
+        self.path = [[-2 for x in range(self.maze.width)] for y in range(self.maze.height)]
+
+    def solve(self, x1, y1, x2, y2):
+        """
+        Использует глубинный поиск для нахождения кратчайшего пути от (x1, y1) до (x2, y2) в лабиринте.
+
+        Parameters:
+        - x1 (int): Координата x начальной точки.
+        - y1 (int): Координата y начальной точки.
+        - x2 (int): Координата x конечной точки.
+        - y2 (int): Координата y конечной точки.
+        """
+        self.path = [[-2 for x in range(self.maze.width)] for y in range(self.maze.height)]
+        visited_solve = [[False for x in range(self.maze.width)] for y in range(self.maze.height)]
+
+        def dfs(x, y, length):
+            """
+            Рекурсивная функция для выполнения глубинного поиска.
+
+            Parameters:
+            - x (int): Текущая координата x.
+            - y (int): Текущая координата y.
+            - length (int): Длина текущего пути.
+
+            Returns:
+            - bool: True, если путь найден, иначе False.
+            """
+            if x == x2 and y == y2:
+                self.path[y][x] = length
+                return True
+            visited_solve[y][x] = True
+
+            directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+            for dx, dy in directions:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < self.maze.width and 0 <= ny < self.maze.height and not visited_solve[ny][nx]:
+                    if dx == 1:
+                        wall = 8
+                    elif dx == -1:
+                        wall = 2
+                    elif dy == 1:
+                        wall = 1
+                    else:
+                        wall = 4
+
+                    if not (self.maze.matrix[y][x] & wall) and dfs(nx, ny, length + 1):
+                        self.path[y][x] = length
+                        return True
+
+            return False
+
+        dfs(x1, y1, 0)
---
+++ main.py
@@ -0,0 +1,52 @@
+from Maze import Maze
+from Player import Player
+from MazeApp import MazeApp
+from Solve import Solver
+from Files import File
+import sys
+import random
+
+sys.setrecursionlimit(10000)
+
+def main():
+    """
+    Основная функция для создания и запуска игры в лабиринте.
+
+    Считывает аргументы командной строки, проверяет их корректность и инициализирует игру.
+    """
+    type_game = ["multi", "solo"]
+
+    if len(sys.argv) != 4:
+        print("Прочитайте инструкцию по вводу и попробуйте еще раз")
+        exit(0)
+
+    width_ = int(sys.argv[1])
+    height_ = int(sys.argv[2])
+
+    if (not sys.argv[3] in type_game) or width_ > 200 or width_ < 2 or height_ > 200 or height_ < 2:
+        print("Прочитайте инструкцию по вводу и попробуйте еще разzzz")
+        exit(0)
+
+    start_x = random.randint(0, width_ - 1)
+    start_y = random.randint(0, height_ - 1)
+
+    finish_x = random.randint(0, width_ - 1)
+    finish_y = random.randint(0, height_ - 1)
+
+    while finish_y == start_y and finish_x == start_x:
+        finish_x = random.randint(0, width_ - 1)
+        finish_y = random.randint(0, height_ - 1)
+
+    CELL_SIZE = min(800 // height_, 1100 // width_)
+
+    maze = Maze(width_, height_)
+    maze.generate_dfs(start_x, start_y)
+    player = Player(start_x, start_y)
+    solver = Solver(maze)
+    solver.solve(start_x, start_y, finish_x, finish_y)
+    file = File(maze)
+    app = MazeApp(maze, CELL_SIZE, start_x, start_y, finish_x, finish_y, solver.path, solver, file, sys.argv[3])
+    app.mainloop()
+
+if __name__ == "__main__":
+    main()
